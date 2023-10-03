85ab36963dc96e4960f434bd788cd19f819e90ec
Shin-SangHyeop <143853428+Shin-SangHyeop@users.noreply.github.com>
2023-10-03 14:36:01 +0900

    [level 2] Title: 짝지어 제거하기, Time: 47.38 ms, Memory: 58.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution
+{
+    public int solution(String s)
+    {
+        int answer = -1;
+        Stack<Character> st = new Stack<>();
+        
+        for(int i=0; i<s.length(); i++){
+            if(st.isEmpty()){
+                st.push(s.charAt(i));
+            }else{
+                if(st.peek() == s.charAt(i)){
+                    st.pop();
+                }else{
+                    st.push(s.charAt(i));
+                }
+            }
+        }
+
+        if(st.isEmpty()){
+            answer = 1;
+        }else{
+            answer = 0;
+        }
+
+        return answer;
+    }
+}
